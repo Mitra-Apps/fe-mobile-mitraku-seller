7072ed62b24d7148dc8eae99e797c0cf61a82e7f
rhmn68 <auliar68@gmail.com>
2024-03-04 13:16:03 +0700

    cek

---
+++ packages/rest_client/lib/src/models/otpconfirmation/resend_otp_response.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'resend_otp_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ResendOTPResponseImpl _$$ResendOTPResponseImplFromJson(
+        Map<String, dynamic> json) =>
+    _$ResendOTPResponseImpl(
+      code: json['code'] as int,
+      message: json['message'] as String,
+    );
+
+Map<String, dynamic> _$$ResendOTPResponseImplToJson(
+        _$ResendOTPResponseImpl instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'message': instance.message,
+    };

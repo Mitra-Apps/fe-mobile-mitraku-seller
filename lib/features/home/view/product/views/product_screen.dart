364ab4f3a545373cb5bb3444561cd85a5482dd5a
rhmn68 <auliar68@gmail.com>
2024-04-02 16:05:38 +0700

    remove back button

--- lib/features/home/view/product/views/product_screen.dart
+++ lib/features/home/view/product/views/product_screen.dart
@@ -86,6 +86,7 @@ class _ProductScreenState extends State<ProductScreen> {
             .titleLarge!
             .copyWith(fontWeight: FontWeight.bold),
       ),
+      automaticallyImplyLeading: false,
     );
   }
 }
